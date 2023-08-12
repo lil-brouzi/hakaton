32059a2786e2fad64543f2efda0853face94c369
lil-brouzi <lil.brouzi@vk.com>
2023-08-12 16:25:09 +0300

    16:25

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import { EVENT } from "./eventBlock";
+// import { EVENT } from "./eventBlock";
 
 
-new EVENT('Сбор мусора', 'dkdkldklkldklkdlkldklkdlkldkldklkldkldklkdlkldklkldkl', 'Смазчиков 3', '01.01.2023', '02.01.2023', '56.85930498828501', '60.619874133537145')
+// new EVENT('Сбор мусора', 'dkdkldklkldklkdlkldklkdlkldkldklkldkldklkdlkldklkldkl', 'Смазчиков 3', '01.01.2023', '02.01.2023', '56.85930498828501', '60.619874133537145')
 
-// Yandex Map
+// // Yandex Map
 let center = [56.8357751144017,60.61140330780871]
 
 function initMap() {
--- src/eventBlock.js
+++ src/eventBlock.js
@@ -1,28 +1,28 @@
-export class EVENT {
-    constructor(name, description, address, startDate, lastDate, coordinateX, coordinateY,) {
-        this.name = name,
-        this.description = description,
-        this.address = address,
-        this.startDate = startDate,
-        this.lastDate = lastDate, 
-        this.coordinateX = coordinateX,
-        this.coordinateY = coordinateY    
-    }
+// export class EVENT {
+//     constructor(name, description, address, startDate, lastDate, coordinateX, coordinateY,) {
+//         this.name = name,
+//         this.description = description,
+//         this.address = address,
+//         this.startDate = startDate,
+//         this.lastDate = lastDate, 
+//         this.coordinateX = coordinateX,
+//         this.coordinateY = coordinateY    
+//     }
 
-    toHTML() {
-        return `
-            <div class="content-event">
-                <img src="/src/assets/eventimg.png" alt="">
-                <div class="content-event-info">
-                    <h2>${this.name}</h2>
-                    <div class="content-event__date">
-                        ${this.startDate} - ${this.lastDate}
-                    </div>
-                    <div class="content-event__descript">
-                        ${this.description}
-                    </div>
-                </div>
-            </div>
-        `
-    }
-}
+//     toHTML() {
+//         return `
+//             <div class="content-event">
+//                 <img src="/src/assets/eventimg.png" alt="">
+//                 <div class="content-event-info">
+//                     <h2>${this.name}</h2>
+//                     <div class="content-event__date">
+//                         ${this.startDate} - ${this.lastDate}
+//                     </div>
+//                     <div class="content-event__descript">
+//                         ${this.description}
+//                     </div>
+//                 </div>
+//             </div>
+//         `
+//     }
+// }
